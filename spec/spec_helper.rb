c16af06ac3550e6d32a80da55349d39d32ff6040
Mike Dalessio <mike@csa.net>
2011-04-20 00:28:45 -0400

    we don't need 'rr', and let's use rspec 2 (because I can't get rspec 1 to install under jruby today)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,4 @@
 require File.dirname(__FILE__) + '/../lib/roleful'
 
 require 'rubygems'
-require 'spec'
-require 'rr'
-
-Spec::Runner.configure do |config|
-  config.mock_with :rr
-end
+require 'rspec'
